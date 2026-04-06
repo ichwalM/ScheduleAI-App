0ee2b88825c8107351a925b5c1c771ee7265cbb3
ichwalM <13020230049@student.umi.ac.id>
2026-04-06 12:03:11 +0800

    fix error admin route

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -14,8 +14,10 @@ class EmailVerificationPromptController extends Controller
      */
     public function __invoke(Request $request): RedirectResponse|View
     {
+        $dashboardRoute = $request->user()->role === 'admin' ? 'admin.dashboard' : 'student.dashboard';
+
         return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(route('dashboard', absolute: false))
+                    ? redirect()->intended(route($dashboardRoute, absolute: false))
                     : view('auth.verify-email');
     }
 }
