657b62d77e34a5a6f02bbf593d3297aa6ff1faac
ichwalM <13020230049@student.umi.ac.id>
2026-04-22 13:02:55 +0800

    update time

--- routes/console.php
+++ routes/console.php
@@ -10,4 +10,4 @@ Artisan::command('inspire', function () {
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('app:send-daily-reminders')
-    ->dailyAt('06:00');
+    ->dailyAt('00:00');
